2ee886f1f2e185b9a987af65faa3c5f18eba18d6
JD <upkanet@gmail.com>
2026-04-18 07:58:33 +0200

    list models

--- checkAI.js
+++ checkAI.js
@@ -4,15 +4,20 @@ import 'dotenv/config';
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 const aiModelName = process.env.AI_MODEL;
 
-const prompt = `
-    Salut, comme t'appelles-tu ?
-`;
+// const prompt = `
+//     Salut, comme t'appelles-tu ?
+// `;
 
-const response = await ai.models.generateContent({
-    model: aiModelName,
-    contents: prompt
-});
+// const response = await ai.models.generateContent({
+//     model: aiModelName,
+//     contents: prompt
+// });
 
-console.log(prompt)
-console.log(response.text)
+// console.log(prompt)
+// console.log(response.text)
+// console.log(ai.models)
 
+console.log("==== Models ====")
+for await (const model of await ai.models.list()){
+    console.log(model.name.split('models/')[1])
+}
